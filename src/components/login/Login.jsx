1a8b093edb82792147741376435c5b2ad0307c28
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-07-26 09:21:29 -0300

    adding an eye icon that shows and hides the password- it needs to finish centering it in the input.

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,18 @@
+import { useState } from 'react';
+import { Eye, EyeOff } from 'react-feather';
 
 const LoginForm = () => {
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <div className="max-w-md mx-auto">
       <h1 className="text-3xl font-bold mb-4">Login</h1>
@@ -14,16 +27,27 @@ const LoginForm = () => {
           placeholder="Enter your email"
         />
       </div>
-      <div className="mb-6">
+      <div className="mb-6 relative flex flex-col">
         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
           Password:
         </label>
-        <input
-          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-          type="password"
-          id="password"
-          placeholder="Enter your password"
-        />
+        <div className='border border-blue-300'>
+            <input
+                className="w-full px-3 py-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={handlePasswordChange}
+                id="password"
+                placeholder="Enter your password"
+            />
+            <div
+            className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer border border-red-300"
+            onClick={togglePasswordVisibility}
+            style={{ top: '50%', transform: 'translateY(-50%)' }}
+            >
+            {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+            </div>
+        </div>
       </div>
       <div>
         <button
@@ -46,4 +70,3 @@ const LoginForm = () => {
 };
 
 export default LoginForm;
-
