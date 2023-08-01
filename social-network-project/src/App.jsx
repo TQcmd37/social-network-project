7b9d742d2c76204391ba0acc9070f558fe406135
TommySosa <tomas.facundo.sosa@gmail.com>
2023-08-01 17:07:40 -0300

    Upload Picture with firebase

--- social-network-project/src/App.jsx
+++ social-network-project/src/App.jsx
@@ -1,13 +1,18 @@
 // import LoginForm from "./components/loginForm/LoginForm"
 
-import Container from "./components/container/Container"
-import LoginForm from "./components/loginForm/LoginForm"
+// import Container from "./components/container/Container"
+// import LoginForm from "./components/loginForm/LoginForm"
 import NavBar from "./components/NavBar"
+import UploadPicture from "./components/uploadPicture/UploadPicture"
 
 function App() {
 
   return (
-    <LoginForm />
+    <>
+    
+    <NavBar />
+    <UploadPicture />
+    </>
   )
 }
 
---
+++ social-network-project/src/components/uploadPicture/UploadPicture.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { storage } from '../../firebase';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+
+const DEFAULT_IMG = 'https://static.vecteezy.com/system/resources/previews/002/519/144/non_2x/social-media-avatar-free-vector.jpg'
+
+export default function UploadPicture(){
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [url, setUrl] = useState(null)
+  const handleFileChange = (event) => {
+    if(event.target.files[0]){
+        setSelectedFile(event.target.files[0]);
+    }
+  };
+  console.log(selectedFile);
+  const handleUpload = async () => {
+    const imageRef = ref(storage, `/user${Math.random()}/image-${Date.now()}`)
+    uploadBytes(imageRef, selectedFile).then(()=> {
+        getDownloadURL(imageRef).then((url) => {
+            setUrl(url)
+        })
+        .catch((error) => {
+            console.log(error.message, "Error obteniendo la imagen");
+        })
+        setSelectedFile(null)
+    }).catch((error) => {
+        console.log(error.message, "Error obteniendo la imagen");
+    })
+  };
+
+  return (
+    <div className="w-full max-w-xs mx-auto">
+      <label className="block mb-2 text-gray-700">Seleccione una imagen:</label>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileChange}
+        className="w-full mb-4 p-2 border rounded"
+        name="profilePicture"
+      />
+      {url ? <img src={url} className="w-32 h-32 rounded-full object-cover mx-auto border"/> : <img src={DEFAULT_IMG} className="w-32 h-32 rounded-full object-cover mx-auto border"/>}
+      <button onClick={handleUpload} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-auto flex my-4'>Subir imagen</button>
+    </div>
+  );
+}
