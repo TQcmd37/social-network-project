07a5e4965eeb3812ba813dbc13e6cf9be951a88f
TommySosa <tomas.facundo.sosa@gmail.com>
2023-08-01 14:52:46 -0300

    Post with axios

--- social-network-api/src/index.js
+++ social-network-api/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import usersRoutes from './routes/users.routes.js';
+import cors from 'cors';
 
 
 const app = express();
@@ -7,6 +8,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json())
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
 app.use('/auth', usersRoutes)
 
