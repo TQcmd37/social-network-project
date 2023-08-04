5a6b792e0320c2e8d221dbc5a1e64024becf0cfe
Rodrigo Peralta <rodrigoperaltamontero@gmail.com>
2023-08-04 14:32:48 -0300

    install and config socket.io-server

--- social-network-api/src/index.js
+++ social-network-api/src/index.js
@@ -2,22 +2,38 @@ import express from 'express';
 import usersRoutes from './routes/users.routes.js';
 import postRoutes from './routes/posts.routes.js'
 import cors from 'cors'
+import { Server } from 'socket.io';
+import http from 'node:http'
 
 
 const app = express();
+app.use(cors({
+  origin: "*" /* 'http://localhost:5173' */
+}))
+
+const httpServer = http.createServer(app)
+
+// Create global chat
+const io = new Server(httpServer, {
+  cors: { 
+    origin: "*"
+  }
+})
+
+
+io.on('connection', (socket)=>{
+  console.log(`Un usuario conectado ${socket.id}`)
+})
 
 const port = 3000;
 
 app.use(express.json())
 
-app.use(cors({
-  origin: 'http://localhost:5173'
-}))
 
 app.use('/auth', usersRoutes)
 
 app.use('/api', postRoutes)
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`El servidor está corriendo en el puerto: ${port}`);
 });
